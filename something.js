4a02fc0baf394492a7c62a31b604529f9ee37218
John Jenson <JRRJenson@gmail.com>
2017-04-19 16:35:06 -0700

    Include both messages

--- something.js
+++ something.js
@@ -1,9 +1,6 @@
 function helloWorld() {
-<<<<<<< c528b479e312396367de4319f2a4827731272cd4
     console.log("HELLO w0rld");
-=======
     console.log("hello everyone")
->>>>>>> Changed message
 }
 
 helloWorld();
